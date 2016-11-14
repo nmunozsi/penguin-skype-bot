91cc55a3f11736c0200d5490f1f0f6b19fdaee25
Andres Zorro <andres.zorro@zemoga.com>
2016-11-14 18:37:39 -0500

    Adding restify route

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 const restify = require("restify");
 const builder = require("botbuilder");
 
-console.log("Hello World");
+// Setup Restify Server
+const server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, function () {
+   console.log("%s listening to %s", server.name, server.url); 
+});
+
+server.get("/", (req, res) => {
+    res.send({hello: "world"});
+    return next();
+})
