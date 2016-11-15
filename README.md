99cfd442a606208d9c7138ea86c9179b7cb4f197
Andres Zorro <andres.zorro@zemoga.com>
2016-11-15 06:54:07 -0500

    Adding bot code

---
+++ README.md
@@ -0,0 +1,2 @@
+Penguin Skype Bot
+=================
--- index.js
+++ index.js
@@ -7,7 +7,24 @@ server.listen(process.env.port || process.env.PORT || 3978, function () {
    console.log("%s listening to %s", server.name, server.url); 
 });
 
+console.log(process.env.APP_ID);
+console.log(process.env.APP_PWD);
+
+const connector = new builder.ChatConnector({
+    appId: process.env.APP_ID,
+    appPassword: process.env.APP_PWD
+});
+
+const bot = new builder.UniversalBot(connector);
+
+// Restify
 server.get("/", (req, res) => {
     res.send({hello: "world"});
     return next();
-})
+});
+server.post('/api/messages', connector.listen());
+
+// Bot
+bot.dialog('/', function (session) {
+    session.send("Hello World");
+});
