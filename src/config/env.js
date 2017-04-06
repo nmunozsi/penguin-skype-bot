6e2fb8ecf0015485568d14859aa79206097cd2d9
Nicolas Muñoz <nmunozsi@gmail.com>
2017-04-06 15:49:36 -0500

    Adapted for PMO

--- src/config/env.js
+++ src/config/env.js
@@ -1,11 +1,11 @@
 module.exports = Object.assign({
     ENV: 'local',
-    SERVER_PORT: 3002,
+    SERVER_PORT: 3004,
     LOG_LEVEL: 'log',
     DATE_FORMAT: 'YY/MM/DD HH:mm:ss',
     HOLIDAYS_API_URL: 'https://habitual-skull.glitch.me/holidays',
-    PENGUIN_REPORT_API: 'https://uitraining.zemoga.com/penguin-report/api',
-    PENGUIN_REPORT_URL: 'https://uitraining.zemoga.com/penguin-report',
+    PENGUIN_REPORT_API: 'https://uitraining.zemoga.com/penguin-pmo-report/api',
+    PENGUIN_REPORT_URL: 'https://uitraining.zemoga.com/penguin-pmo-report',
     DB_NAME: 'penguin-bot-db',
     DB_PATH: '.'
 }, process.env);
