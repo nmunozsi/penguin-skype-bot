2cea29db7046a03666fabda9e4ff3f502968f3e8
Nicolas Muñoz Silva <nmunozsi@gmail.com>
2018-02-19 15:47:20 -0500

    Update env.js
    updated API URL

--- src/config/env.js
+++ src/config/env.js
@@ -4,7 +4,7 @@ module.exports = Object.assign({
     LOG_LEVEL: 'log',
     DATE_FORMAT: 'YY/MM/DD HH:mm:ss',
     HOLIDAYS_API_URL: 'https://habitual-skull.glitch.me/holidays',
-    PENGUIN_REPORT_API: 'https://uitraining.zemoga.com/penguin-pmo-report/api',
+    PENGUIN_REPORT_API: 'https://uitraining.zemoga.com/penguin-report/api?department=PMO',
     PENGUIN_REPORT_URL: 'https://uitraining.zemoga.com/penguin-pmo-report',
     DB_NAME: 'penguin-bot-db',
     DB_PATH: '.'
